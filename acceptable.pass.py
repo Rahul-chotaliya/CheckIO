874be1b4b9b9a8750b559c618f986e038ac2b120
Rahul-chotaliya <128304248+Rahul-chotaliya@users.noreply.github.com>
2023-05-01 10:40:07 +0530

    Add files via upload

---
+++ acceptable.pass.py
@@ -0,0 +1,26 @@
+#accepteble password
+# Taken from mission Acceptable Password I
+
+def is_acceptable_password(password: str) -> bool:
+    if len(password)>6 :
+        for k in password:
+            if k in "0123456789":
+                return True
+        else:
+            return False       
+    else:
+        return False
+    
+
+
+print("Example:")
+print(is_acceptable_password("short"))
+
+# These "asserts" are used for self-checking
+assert is_acceptable_password("short") == False
+assert is_acceptable_password("muchlonger") == False
+assert is_acceptable_password("ashort") == False
+assert is_acceptable_password("muchlonger5") == True
+assert is_acceptable_password("sh5") == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
